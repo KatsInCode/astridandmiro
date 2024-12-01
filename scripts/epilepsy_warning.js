78dadc70f5f04e16d67bc702e992f65b2039e878
Henson <catinboxed@gmail.com>
2024-12-01 18:24:29 +0300

    Added utf-8 meta tags while removing some unnecessary tags. Started work on the ocean page.

--- scripts/epilepsy_warning.js
+++ scripts/epilepsy_warning.js
@@ -23,7 +23,7 @@ function setCookie(cname, cvalue, exdays) {
 
 let alerted = getCookie("epilepsy") ? parseInt(getCookie("epilepsy")) : 0;
 
-if (alerted == 0) {
+if (alerted != 1) {
     alert("This website may contain epilepsy inducing visual content. Browse at your own risk!");
     setCookie("epilepsy", 1, 7);
 }
